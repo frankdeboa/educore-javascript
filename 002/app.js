2cb696bf62eb35ff7fd8c326a64a55fbaf80057f
Frank Tramontin <frankdavi@gmail.com>
2022-10-07 15:06:25 -0300

    variaveis

---
+++ 002/app.js
@@ -0,0 +1,43 @@
+console.log("Encontro 2")
+
+/*/
+Sintaxe javascripts, variáveis e tipos de dados
+*/
+
+//variáveis
+
+nome = "Frank"
+console.log(nome)
+var nome = "Davi"
+console.log(nome)
+var nome = "Tramontin"
+console.log(nome)
+let sobrenome = "Travon"
+console.log(sobrenome)
+
+if (10 > 3) {
+    console.log("10 maior que 3")
+    var idade = 21
+    console.log(idade)
+}
+
+let midia = "tv"
+console.log(midia)
+
+//tipos de dados
+let idade2
+let apelido
+
+idade2 = 15
+apelido = "deboa"
+console.log(typeof idade)
+console.log(typeof apelido)
+
+let concorda = true
+console.log(typeof concorda)
+
+let carros = ['gol', 'corsa', 'uno']
+console.log(typeof carros)
+
+let pessoa = {'nome':'frank', 'idade':37}
+console.log(typeof pessoa)
