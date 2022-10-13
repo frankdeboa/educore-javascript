e511538da9fc6d0b4667c653092fc7c90749d7b6
Frank Tramontin <frankdavi@gmail.com>
2022-10-13 14:37:05 -0300

    concatenação

---
+++ app.js
@@ -0,0 +1,16 @@
+/*Operadores*/
+
+let n1, n2
+
+n1 = 10
+n2 = 2
+
+document.write("N1 + N2 = " + (n1+n2) + "<br>")
+document.write("N1 - N2 = " + (n1-n2) + "<br>")
+document.write("N1 / N2 = " + n1/n2 + "<br>")
+document.write("N1 * N2 = " + n1*n2 + "<br>")
+
+let nome, sobrenome
+nome = "João"
+sobrenome = " Paulo"
+document.write(nome + sobrenome + "<br") //concatenação
