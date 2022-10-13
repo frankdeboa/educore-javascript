910a5d351d6ac382fdde6b9342229701d5321c91
Frank Tramontin <frankdavi@gmail.com>
2022-10-13 15:17:51 -0300

    operadores + incrementos

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ document.write('N1 ** N2 = ' + n1**n2 + "<br>") //operador de potência
 
 document.write (Math.sqrt(16))
 
-function mensagem(){
-    
+function somar(){ //conjunto de código, que damos um nome
+    let v1, v2
+    v1 = parseFloat(document.getElementById("valor1").value)
+    v2 = parseFloat(document.getElementById("valor2").value)
+    document.getElementById("resultado").value = v1 + v2
 }
---
+++ app2.js
@@ -0,0 +1,7 @@
+// Operadores de Incremento / Decremento
+
+
+let n1 = 10
+
+n1 = n1 +1
+document.write(n1 + "<br>")
