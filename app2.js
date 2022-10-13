d8118e7856aef9371e160d12ac6164b809c76d58
Frank Tramontin <frankdavi@gmail.com>
2022-10-13 15:32:12 -0300

    operadores aritmeticos

--- app2.js
+++ app2.js
@@ -1,7 +1,33 @@
 // Operadores de Incremento / Decremento
 
 
-let n1 = 10
+let n1 = 10, n2 = 2
 
-n1 = n1 +1
+n1 = n1 + 1
 document.write(n1 + "<br>")
+document.write(n1++ + "<br>")
+//se o ++ antes ele incrementa e mostra se está depois ele mostra
+document.write(n1 + "<br>")
+
+document.write(--n1 + "<br>") //aqui está retirando
+//ele mostra o resultado original
+
+// let i = 0
+// while(i < 5){
+//     document.write("Olá <br>")
+//     i++
+// }
+
+//operadores relacionais
+
+document.write((n1 > n2) + "<br>")
+document.write((n1 < n2) + "<br>")
+document.write((n1 != n2) + "<br>")
+document.write((n1 == n2) + "<br>")
+document.write((n1 >= 10) + "<br>")
+document.write((n1 <= 6) + "<br>")
+document.write((n1 == "10") + "<br>")
+
+let n3 = "10"
+document.write((n1 == n3) + "<br>")
+document.write((n1 === n3))
