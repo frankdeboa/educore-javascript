acc3bed65cc2a8400ef7cfc721a068873e66554d
Frank Tramontin <frankdavi@gmail.com>
2022-10-13 14:52:33 -0300

    formulário html

--- app.js
+++ app.js
@@ -13,4 +13,14 @@ document.write("N1 * N2 = " + n1*n2 + "<br>")
 let nome, sobrenome
 nome = "João"
 sobrenome = " Paulo"
-document.write(nome + sobrenome + "<br") //concatenação
+document.write(nome + sobrenome + "<br>") //concatenação
+
+document.write('N1 % N2 = ' + n1%n2 + "<br>") //operador módulo (resto da divisão)
+
+document.write('N1 ** N2 = ' + n1**n2 + "<br>") //operador de potência
+
+document.write (Math.sqrt(16))
+
+function mensagem(){
+    
+}
